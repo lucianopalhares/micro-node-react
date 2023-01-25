979e0ca356146f945fe822a95248b35f64fa7ac5
Luciano Palhares <luciano.palhares.ext@infracommerce.com.br>
2023-01-25 15:54:28 -0300

    adicionando env file

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import PostCreate from './PostCreate'
-import PostList from './PostList';
+import PostList from './PostList'
 
 const App = () => {
+
     return ( 
         <div className='container'>
-            <h1>Create Post !!!!!!!</h1>
+            <h1>Create Post <small>- Versão: {process.env.REACT_APP_VERSAO}</small></h1>
             <PostCreate />
             <hr/>
             <h1>Posts</h1>
